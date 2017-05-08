f251bc4fd835eae3c4c61f4df6bbcbf35ab62a47
Gary Grossi <GsGr4400@gmail.com>
2017-05-07 21:07:59 -0400

    inital commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        // Get rigidbody
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    // Pre physics
+    private void FixedUpdate()
+    {
+        // Get horizontal and vertical input
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        // Create a Vector2 for movement based on inputs
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        // Apply the movement to the character
+        rigidBody.AddForce(movement * speed);
+    }
+}
