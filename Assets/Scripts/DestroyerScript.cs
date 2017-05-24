3ea11f484b66795d5d1e35acaa1120a2cfc24ea2
garygrossi <GsGr4400@gmail.com>
2017-05-24 14:18:37 -0400

    Bug fixes
    Switching from EditorSceneManager to SceneManager.

---
+++ Assets/Scripts/DestroyerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyerScript : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Grass")
+        {
+            collision.gameObject.transform.position += new Vector3(0, 11.5f);
+        }
+        else if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Replay.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Replay : MonoBehaviour {
+
+    public Button replayButton;
+    public Text countCreamText;
+
+    void Start()
+    {
+        countCreamText.text = Data.Score.ToString();
+        Button btn = replayButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("You have clicked the button!");
+    }
+}
