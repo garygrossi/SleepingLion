aa886c9c96369459663eb6f8d6beb6c8baf32601
garygrossi <GsGr4400@gmail.com>
2017-05-29 13:14:36 -0400

    Adding lifebar display

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour {
+
+    public GameObject[] health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Data.Life < 3)
+        {
+            health[2].SetActive(false);
+        }
+
+        if (Data.Life < 2)
+        {
+            health[2].SetActive(false);
+            health[1].SetActive(false);
+        }
+
+        if (Data.Life < 1)
+        {
+            health[2].SetActive(false);
+            health[1].SetActive(false);
+            health[0].SetActive(false);
+        }
+    }
+}
